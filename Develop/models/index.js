bc9b70d65573bc81768c66beb64f1033dfdf06fb
Diane Tritchler <dtritchler92@gmail.com>
2022-02-09 23:02:59 -0600

    final - added readme

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tag',
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_name',
+  foreignKey: 'tag_id'
+});
 
+// export these 
 module.exports = {
   Product,
   Category,
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# E-commerce Back End Starter Code
+# E-commerce
+
+## Description {#description}
+
+databases for your products
+
+## Installation Instructions {#installation}
+
+download the github use this json example
+example json for product
+{
+      "product_name": "Basketball",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
+    }
+
+## Examples for use {#examples}
+if you have a business, you will love this program
+
+
+
+
+## Questions, comments concerns? {#questions}
+
+Please get in touch with me via github or my email below.
+
+[Github](https://www.github.com/DianeTritchler)
+
+dtritchler@gmail.com
